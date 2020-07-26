57c94ce57c864c15a2936612978ab9e0404a6c2b
smahamkl <tzones2006@gmail.com>
2020-07-25 23:20:23 -0700

    practice samples

---
+++ Practice2.scala
@@ -0,0 +1,27 @@
+object Practice1{
+
+    def toInt(value: String): Option[Int] = {
+        try {
+            Some(Integer.parseInt(value))
+        }catch{
+            case ex:NumberFormatException => Some(0)
+        }
+    }
+    def main(args: Array[String])
+    {
+        val l = List("12", "23", "34", "45", "5","6", "", "None")
+
+        val i = toInt("sdfasdasdasd") match {
+            case Some(i1) => i1
+            case None => 0
+        }
+
+        println(i)
+        println("---------------------------")
+
+        val k = l.flatMap(toInt(_)).sum
+
+        println(k)
+        //k.foreach(println)
+    } 
+}
