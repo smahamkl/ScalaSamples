5dce951c7963d52c85aeca808be9b735b435f1b5
smahamkl <tzones2006@gmail.com>
2020-08-30 23:47:10 -0700

    added spell checker

--- FriendsPairingSample.py
+++ FriendsPairingSample.py
@@ -2,7 +2,8 @@ import sys
 '''
 Friends Pairing Problem
 18-02-2017
-Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. 
+Given n friends, each one can remain single or can be paired up with some other friend. 
+Each friend can be paired only once. 
 Find out the total number of ways in which friends can remain single or can be paired up.
 
 Examples :
---
+++ Practice3.scala
@@ -0,0 +1,32 @@
+object Practice1{
+
+    def findLCA(node:MyNode, val1: Int, val2: Int): Option[MyNode] = {
+        if(node.value == val1 || node.value == val2) Some(node)
+        else(
+            if(node.left != None) findLCA(node.left.get, val1, val2)  else None,
+            if(node.right != None) findLCA(node.right.get, val1, val2) else None
+        ) match {
+            case (Some(l), Some(r)) => Some(node)
+            case (Some(l), None) => Some(l)
+            case (None, Some(r)) => Some(r)
+            case (None, None) => None
+        }
+    }
+
+    def main(args: Array[String])
+    {
+            val tree = MyNode(1,
+      Some(MyNode(2,
+        Some(MyNode(4, None, None)),
+        Some(MyNode(5, None, None))
+      )),
+      Some(MyNode(3,
+        Some(MyNode(6, None, None)),
+        Some(MyNode(7, None, None))
+      ))
+            )
+      println(findLCA(tree, 5, 6).get)
+    } 
+
+    case class MyNode(value: Int, left: Option[MyNode], right:Option[MyNode])
+}
