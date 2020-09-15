843ab831e6b3b5b5531cfebb5d4e68f5a9b92a00
Siva M <sivam@sivas-imac.lan>
2020-09-14 17:29:37 -0700

    added House robber problem

--- ThreeSum.py
+++ ThreeSum.py
@@ -2,6 +2,22 @@ from typing import List
 import copy
 from itertools import combinations
 
+'''
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+Note:
+
+All numbers will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6], [1,3,5], [2,3,4]]
+'''
 class Solution:
     def validArr(self, arr:List[int]) -> bool:
         for i in range(1, len(arr)):
@@ -42,6 +58,7 @@ class Solution:
             self.combSum(k, n, copy.deepcopy(arr), finalArr)
             
         return finalArr
+    #---working function to first form combinations of numbers and then filter based on the sum
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         return filter(lambda x: sum(x) == n, combinations(range(1, 10), r=k))
 
