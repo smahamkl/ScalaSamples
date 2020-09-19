7412a8e09379c9704abdf4d221e66a74323794d5
Siva M <sivam@sivas-imac.lan>
2020-09-19 12:21:05 -0700

    sep challenge problems

---
+++ SeqientialDigits.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        digitySeq="123456789"
+        if low >= high:
+            if str(low) in digitySeq:
+                return low
+            else:
+                return []
+
+        l = len(str(low))
+        r = len(str(high))
+        res = []
+        for i in range(l, r+1):
+            for j in range(len(digitySeq)-i+1):
+                num = int(digitySeq[j:i+j])
+                if (num >= low) & (num <= high):
+                    res.append(num)
+
+        return res
+        
+
+sol = Solution()
+print(sol.sequentialDigits(100,300))
+print(sol.sequentialDigits(1000,13000))
+print(sol.sequentialDigits(10,10))
