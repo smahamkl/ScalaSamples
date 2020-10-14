9394937c0486dd1195447661b51f4d2bc39cb8b4
smahamkl <tzones2006@gmail.com>
2020-10-13 22:45:14 -0700

    Oct challenge problems

---
+++ oct2020challenge/SortList.py
@@ -0,0 +1,63 @@
+from typing import List
+'''
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if head == None:
+            return head
+        elif head.next == None:
+            return head
+        return self.divideList(head)
+    
+    def divideList(self, head:ListNode) -> ListNode:
+        slow_ptr = fast_ptr = head
+        #print(head.val)
+        if head.next == None:
+            return head
+        while fast_ptr != None and fast_ptr.next != None:
+            tmp  = slow_ptr
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+        tmp.next = None
+        i = self.divideList(head)
+        j = self.divideList(slow_ptr)
+        return self.merge(i, j)
+    def merge(self, i, j):
+        """
+                 This method mainly achieves the combined operation
+                 The process of combining is to start from i and j. It is to start from the beginning and the middle.
+                 Finally return to the head
+        :param i:ListNode
+        :param j:ListNode
+        :return:
+        """
+        TempNode = ListNode(0)
+        temp = TempNode
+        while i is not None and j is not None:
+            if i.val <= j.val:
+                temp.next = i
+                i = i.next
+            else:
+                temp.next = j
+                j = j.next
+            temp = temp.next
+        if i is not None:
+            temp.next = i
+        if j is not None:
+            temp.next = j
+        return TempNode.next
+
+
+#node = ListNode(4)
+#node.next = ListNode(2)
+#node.next.next = ListNode(1)
+#node.next.next.next = ListNode(3)
+print(Solution().sortList(None).val)
--- others/MergeKLists.py
+++ others/MergeKLists.py
@@ -19,42 +19,42 @@ class Solution:
             tmp.append(headN)
         return tmp
     #-----this is by using a priority queue -----
-    def mergeKLists(self, lists):
-            """
-            :type lists: List[ListNode]
-            :rtype: ListNode
-            """
-            head = point = ListNode(0)
-            q = PriorityQueue()
-            for l in lists:
-                if l:
-                    q.put((l.val, l))
-            while not q.empty():
-                val, node = q.get()
-                point.next = ListNode(val)
-                point = point.next
-                node = node.next
-                if node:
-                    q.put((node.val, node))
-            return head.next
+    # def mergeKLists(self, lists):
+    #         """
+    #         :type lists: List[ListNode]
+    #         :rtype: ListNode
+    #         """
+    #         head = point = ListNode(0)
+    #         q = PriorityQueue()
+    #         for l in lists:
+    #             if l:
+    #                 q.put((l.val, l))
+    #         while not q.empty():
+    #             val, node = q.get()
+    #             point.next = ListNode(val)
+    #             point = point.next
+    #             node = node.next
+    #             if node:
+    #                 q.put((node.val, node))
+    #         return head.next
     #----working - brut force way -------
-    # def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-    #     res = []
-    #     for list in lists:
-    #         while list != None and list.val != 0:
-    #             res.append(list.val)
-    #             list = list.next
-    #     if len(res) == 0:
-    #         return None
-    #     res = sorted(res)
-    #     tmpNode = resNode = ListNode()
-    #     print(res)
-    #     for i in range(len(res)):
-    #         resNode.val = res[i]
-    #         if i < (len(res)-1):
-    #             resNode.next = ListNode()
-    #             resNode = resNode.next
-    #     return tmpNode
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        res = []
+        for list in lists:
+            while list != None and list.val != 0:
+                res.append(list.val)
+                list = list.next
+        if len(res) == 0:
+            return None
+        res = sorted(res)
+        tmpNode = resNode = ListNode()
+        print(res)
+        for i in range(len(res)):
+            resNode.val = res[i]
+            if i < (len(res)-1):
+                resNode.next = ListNode()
+                resNode = resNode.next
+        return tmpNode
         
 sol = Solution()
 matrix = sol.buildListNode([[1,4,5],[1,3,4],[2,6]])
