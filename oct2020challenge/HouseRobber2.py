13a6ed22975d7bdc5885972491fec2f3ace72721
smahamkl <tzones2006@gmail.com>
2020-10-15 11:50:54 -0700

    oct challenge problems

--- oct2020challenge/HouseRobber2.py
+++ oct2020challenge/HouseRobber2.py
@@ -6,7 +6,6 @@ That means the first house is the neighbor of the last one. Meanwhile, adjacent 
 
 Given a list of non-negative integers nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
- 
 
 Example 1:
 
