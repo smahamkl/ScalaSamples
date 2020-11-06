cc29335e4ce44bffd1773a063db31abdb7f0c28c
smahamkl <tzones2006@gmail.com>
2020-11-05 16:44:16 -0800

    nov challenge problems

--- nov2020challenge/MinCostMovingChips.py
+++ nov2020challenge/MinCostMovingChips.py
@@ -1,41 +1,39 @@
 from typing import List
 from queue import PriorityQueue
 import collections
+'''
+We have n chips, where the position of the ith chip is position[i].
 
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+ 
+Example 1:
+
+
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+'''
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        if len(position) <= 1:
+        #---idea is to bring all chips to one of the two positions (position % 2) since 
+        #---switching cost for two positions is 0, and then finally taking the minimum
+        #---count of chips from each of the positions
+        num_map = {x:cnt for x,cnt in collections.Counter([x%2 for x in position]).items()}
+        if len(num_map.keys()) == 1:
             return 0
-
-        max_val = max(position)
-        num_map = {x:cnt for x,cnt in collections.Counter(position).items()}
-
-        print(num_map)
-        
-        q = PriorityQueue()
-        for k,v in num_map.items():
-            q.put((v, k))
-        
-        res = 0
-        if len(num_map.keys()) > 1:
-            while not q.empty():
-                tot,pos = q.get()
-                newtot, newpos = q.get()
-                pos, newpos = pos%2, newpos%2
-                print(tot, pos, newtot, newpos)
-                if abs(newpos - pos) == 1:
-                    res += tot
-
-                if q.empty():
-                    break
-                else:
-                    q.put((tot+newtot, newpos))
-        
-        return res
-
-
+        else:
+            return min(num_map.values())
 
 sol = Solution()
-#print(sol.minCostToMoveChips([2,2,2,3,3]))
-#print(sol.minCostToMoveChips([1,1000000000]))
+print(sol.minCostToMoveChips([2]))
+print(sol.minCostToMoveChips([2,2,2,3,3,4,4,4]))
+print(sol.minCostToMoveChips([1,1000000000]))
 print(sol.minCostToMoveChips([1,2,3]))
+print(sol.minCostToMoveChips([2,2,2,2,2,2,2]))
