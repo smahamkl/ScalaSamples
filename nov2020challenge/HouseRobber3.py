340db066723c104d6fd7a83520e0c21912965f65
smahamkl <tzones2006@gmail.com>
2020-11-23 23:17:39 -0800

    nov challenge prolems

--- nov2020challenge/HouseRobber3.py
+++ nov2020challenge/HouseRobber3.py
@@ -1,5 +1,26 @@
 from typing import List
+'''
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, 
+called the "root." Besides the root, each house has one and only one parent house. 
+After a tour, the smart thief realized that "all houses in this place forms a binary tree". 
+It will automatically contact the police if two directly-linked houses were broken into on the same night.
 
+Determine the maximum amount of money the thief can rob tonight without alerting the police
+
+Example 1:
+
+Input: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+Output: 7 
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+
+'''
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
