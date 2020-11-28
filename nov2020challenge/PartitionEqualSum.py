817244736c9240bc223d2f28fc05a639bdef103f
Sivaprasad Mahamkali <sivam@sivaprasadsimac.lan>
2020-11-28 11:05:06 -0800

    nov challenge problems

--- nov2020challenge/PartitionEqualSum.py
+++ nov2020challenge/PartitionEqualSum.py
@@ -1,47 +1,53 @@
 from typing import List
 from collections import Counter
+'''
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+Solution:
+----------
+Using DP and memozation technique to store the intermedia states by index and the sum
+'''
 class Solution:
-    def checkifpartition(self, nums, i, sum, target) -> bool:
+    def checkifpartition(self, nums, i, sum, target, partmap) -> bool:
         if sum == target:
             return True
         elif i >= len(nums):
             return False
+        elif partmap.get((i, sum), None) != None:
+            return partmap[(i, sum)]
         else:
-            return (self.checkifpartition(nums, i+1, nums[i]+sum, target) or self.checkifpartition(nums, i+1, sum, target))
+            val = (self.checkifpartition(nums, i+1, nums[i]+sum, target, partmap) or self.checkifpartition(nums, i+1, sum, target, partmap))
+            partmap[(i, sum)] =val
+            return val
+
     def canPartition(self, nums: List[int]) -> bool:
         arr_sum = sum(nums)
         if arr_sum % 2 == 1 or arr_sum == 0:
             return False
         arr_sum /= 2
-        nums = sorted(nums)
-        i = 1
-        while i < len(nums):
-            if nums[i] == nums[i-1]:
-                arr_sum -= nums[i]
-                nums.pop(i-1)
-                nums.pop(i-1)
-                continue
-            i += 1
-        print(nums, arr_sum)
-                
-
-
-        # nums = [x[0] for x in Counter(nums).items() if x[1] % 2 == 1]
-        # if len(nums) == 0:
-        #     return True
-        
-        # arr_sum = int(arr_sum / 2)
-        # return self.checkifpartition(nums, 0, 0, arr_sum)
+        return self.checkifpartition(nums, 0, 0, arr_sum, {})
 
 sol  = Solution()
-#print(sol.canPartition([1,5,11,5]))
-# print(sol.canPartition([1,2,3,5]))
+print(sol.canPartition([1,5,11,5]))
+print(sol.canPartition([1,2,3,5]))
 print(sol.canPartition([4,3,1,6]))
-#print(sol.canPartition([6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]))
-# print(sol.canPartition([100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-# 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-# 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-# 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-# 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
-# 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97]))
+print(sol.canPartition([6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]))
+print(sol.canPartition([100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97]))
