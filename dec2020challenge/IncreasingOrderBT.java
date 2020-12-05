60d9b0a62bbd91459b9089088be880abf2fb4409
smahamkl <tzones2006@gmail.com>
2020-12-05 11:52:21 -0800

    dec challenge problems

--- dec2020challenge/IncreasingOrderBT.java
+++ dec2020challenge/IncreasingOrderBT.java
@@ -1,3 +1,6 @@
+/*
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+*/
 class IncreasingOrderBT {
 
     // Definition for a binary tree node.
