f288cf7eb34f62ef5cfc0fbb035711b1c3029575
smahamkl <tzones2006@gmail.com>
2021-01-13 23:14:08 -0800

    jan challenge problems

---
+++ jan2021challenge/BoatsToSavePeople.py
@@ -0,0 +1,11 @@
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        pass
+    
+
+
+sol = Solution()
+print(sol.numRescueBoats([3,2,2,1], 3))
