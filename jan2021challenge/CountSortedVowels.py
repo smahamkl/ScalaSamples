c82fc12d691458a96d3038206eeb7ef36dcf0e80
smahamkl <tzones2006@gmail.com>
2021-01-17 23:39:30 -0800

    jan challenge problems

--- jan2021challenge/CountSortedVowels.py
+++ jan2021challenge/CountSortedVowels.py
@@ -14,18 +14,25 @@ Explanation: The 5 sorted strings that consist of vowels only are ["a","e","i","
 class Solution:
     def __init__(self):
         self.res = 0
-    def fillWords(self, n, i, v, vi, word):
+    def fillWords(self, n, i, v):
         if i == n:
             #print(word)
             self.res+=1
         elif i <= n-1:
-            for idx in range(vi, len(v)):
-                self.fillWords(n, i+1, v[idx:], vi, word+v[idx] )
+            for idx in range(len(v)):
+                self.fillWords(n, i+1, v[idx:])#, word+v[idx] )
 
     def countVowelStrings(self, n: int) -> int:
         v = ['a', 'e', 'i', 'o','u']
-        self.fillWords(n,0, v, 0, "")
+        self.fillWords(n,0, v)
         return self.res
 
+    def countVowelStrings_alternative(self, n: int) -> int:
+        a = [1] * 5
+        for _ in range(1, n):
+            for i in range(1, 5):
+                a[i] += a[i-1]
+        return sum(a)
+
 sol = Solution()
-print(sol.countVowelStrings(1))
+print(sol.countVowelStrings(33))
