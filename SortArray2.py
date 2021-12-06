6ec974bcca2294308857438272cbf7405e67dfca
smahamkl <tzones2006@gmail.com>
2021-12-05 20:01:50 -0800

    sort array in place problem

--- SortArray2.py
+++ SortArray2.py
@@ -7,7 +7,7 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/160
 from math import inf
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates1(self, nums: List[int]) -> int:
         
         # Init
         prev = inf
@@ -29,13 +29,32 @@ class Solution:
                 del nums[i]
       
         return len(nums)
+    
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        if len(nums) <= 2:
+            return len(nums)
+        count = 1
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i-1]:
+                count += 1
+            else:
+                count = 1
+            
+            if count > 2:
+                del nums[i]
+            else:
+                i += 1
+        
+        return len(nums)
 
 sol = Solution()
-#print(sol.removeDuplicates([1,1,1,2,2,3]))
-print(sol.removeDuplicates([1,1,1,1,1,1,2]))
-# print(sol.removeDuplicates([1,2]))
-# print(sol.removeDuplicates([1,2,2,3]))
-# print(sol.removeDuplicates([1,1,1,1,1,1,1,1]))
-# print(sol.removeDuplicates([1,2,3,4,5]))
+print(sol.removeDuplicates([1,1,1,2,2,3]))
+#print(sol.removeDuplicates([1,1,1,1,1,1,2]))
+#print(sol.removeDuplicates([1,2]))
+#print(sol.removeDuplicates([1,2,2,3]))
+#print(sol.removeDuplicates([1,1,1,1,1,1,1,1]))
+#print(sol.removeDuplicates([1,2,3,4,5]))
 # print(sol.removeDuplicates([1]))
 # print(sol.removeDuplicates([]))
