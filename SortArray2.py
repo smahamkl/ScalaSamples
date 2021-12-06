66ffe9ba569d3f1916ce5d76b5f637dcc9b48d2a
smahamkl <tzones2006@gmail.com>
2021-12-05 19:01:11 -0800

    Sort Array 2

---
+++ SortArray2.py
@@ -0,0 +1,38 @@
+from typing import List
+'''
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/1605178/Py3Py-Simple-solution-using-for-loop-and-del-on-list-w-comments
+'''
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # similar to LC26
+        n = len(nums)
+        if n <= 1: return n
+        pos = 1
+        count = 1
+        times = 1
+        for i in range(1, n):
+            if nums[i] != nums[i - 1]:
+                nums[pos] = nums[i]
+                pos += 1
+                count += 1
+                times = 1
+            else:
+                if times == 1:
+                    nums[pos] = nums[i]
+                    pos += 1
+                    count += 1
+                    times += 1
+        print(nums)
+        return count
+
+sol = Solution()
+#print(sol.removeDuplicates([1,1,1,2,2,3]))
+print(sol.removeDuplicates([1,1,1,1,1,1,2]))
+# print(sol.removeDuplicates([1,2]))
+# print(sol.removeDuplicates([1,2,2,3]))
+# print(sol.removeDuplicates([1,1,1,1,1,1,1,1]))
+# print(sol.removeDuplicates([1,2,3,4,5]))
+# print(sol.removeDuplicates([1]))
+# print(sol.removeDuplicates([]))
