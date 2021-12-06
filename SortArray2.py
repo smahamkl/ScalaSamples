9853e20056bb8d0b18393aae362c361b2612c7c9
smahamkl <tzones2006@gmail.com>
2021-12-05 19:47:06 -0800

    sort array in place problem

--- SortArray2.py
+++ SortArray2.py
@@ -4,28 +4,31 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 
 https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/1605178/Py3Py-Simple-solution-using-for-loop-and-del-on-list-w-comments
 '''
+from math import inf
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # similar to LC26
-        n = len(nums)
-        if n <= 1: return n
-        pos = 1
-        count = 1
-        times = 1
-        for i in range(1, n):
-            if nums[i] != nums[i - 1]:
-                nums[pos] = nums[i]
-                pos += 1
-                count += 1
-                times = 1
-            else:
-                if times == 1:
-                    nums[pos] = nums[i]
-                    pos += 1
-                    count += 1
-                    times += 1
-        print(nums)
-        return count
+        
+        # Init
+        prev = inf
+        count = 0
+        
+        # from last to first element
+        for i in range(len(nums)-1,-1,-1):
+           
+            # Reset Count
+            if prev != nums[i]:
+                count = 0
+            
+            # Increment count for current number
+            count += 1
+            prev = nums[i] # previous is current
+            
+            # If count if more than 2 delete
+            if count > 2:
+                del nums[i]
+      
+        return len(nums)
 
 sol = Solution()
 #print(sol.removeDuplicates([1,1,1,2,2,3]))
