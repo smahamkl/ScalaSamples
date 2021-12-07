e4087c14585518d849130ff3c95c8c7e7c491631
smahamkl <tzones2006@gmail.com>
2021-12-07 10:10:29 -0800

    minor comments add

--- sep2020challenge/ContainsDuplicate.py
+++ sep2020challenge/ContainsDuplicate.py
@@ -1,12 +1,14 @@
 from typing import List
 
 '''
+LeetCode 220. Contains Duplicate III
+
 Given an array of integers, find out whether there are two distinct indices i and j in the array such 
 that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
 
 Example 1:
 
-Input: nums = [1,2,3,1], k = 3, t = 0
+Input: nums = [1,2,3,1], idxdiff = 3, valdiff = 0
 Output: true
 Example 2:
 
