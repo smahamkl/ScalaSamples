4728dd51a00bc0bb31b80a6c45c47566b79890f6
smahamkl <tzones2006@gmail.com>
2021-12-10 09:27:01 -0800

    car pooling problem

--- sep2020challenge/MissingPositive.py
+++ sep2020challenge/MissingPositive.py
@@ -21,24 +21,36 @@ Your algorithm should run in O(n) time and uses constant extra space.
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        num_map={}
-        max_val = 0
-        for i in range(len(nums)):
-            if max_val < nums[i]:
-                max_val = nums[i]
 
-            if (nums[i] not in num_map) and (nums[i] > 0):
-                num_map[nums[i]]=1
+        maxnum = min(max(nums), len(nums))
+        print(maxnum)
+        num_arr = [0 for x in range(maxnum+2)]
+
+        for i in nums:
+            num_arr[i] = 1
         
-        print(num_map)
-        for i in range(1, max(max_val+2, 2)):
-            if i not in num_map:
+        for i in range(1, len(num_arr)):
+            if num_arr[i] == 0:
                 return i
+
+        # num_map={}
+        # max_val = 0
+        # for i in range(len(nums)):
+        #     if max_val < nums[i]:
+        #         max_val = nums[i]
+
+        #     if (nums[i] not in num_map) and (nums[i] > 0):
+        #         num_map[nums[i]]=1
+        
+        # print(num_map)
+        # for i in range(1, max(max_val+2, 2)):
+        #     if i not in num_map:
+        #         return i
 
 
 
 sol = Solution()
-print(sol.firstMissingPositive([7,8,9,11,12]))
-print(sol.firstMissingPositive([3,4,-1,1]))
-print(sol.firstMissingPositive([1,2,0]))
-print(sol.firstMissingPositive([10000, 200000]))
+#print(sol.firstMissingPositive([7,8,9,11,12]))
+#print(sol.firstMissingPositive([3,4,-1,1]))
+#print(sol.firstMissingPositive([1,2,0]))
+print(sol.firstMissingPositive([2147483647]))
