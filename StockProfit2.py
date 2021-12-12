8a76fb4f677a286e042176c04a9136757a83ab32
smahamkl <tzones2006@gmail.com>
2021-12-11 23:58:23 -0800

    added few problems

---
+++ StockProfit2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #--valley peak approach
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+
+
+        return profit
+
+
+sol = Solution()
+print(sol.maxProfit([7,1,5,3,6,4]))       
+print(sol.maxProfit([1,2,3,4,5]))
+print(sol.maxProfit([7,6,4,3,1]))
