62b6b27dc409994f43f27bfaf03f79b9b1eaa416
smahamkl <tzones2006@gmail.com>
2022-01-22 18:47:37 -0800

    Numbers disappeared in an array problem

---
+++ NumsDisappearedArray.scala
@@ -0,0 +1,46 @@
+import scala.math._
+import scala.util.Sorting
+import scala.collection.mutable.HashMap
+
+/*
+LeetCode - 448
+
+Find All Numbers Disappeared in an Array
+*/
+
+object Solution extends App{
+    def findDisappearedNumbers(nums: Array[Int]): List[Int] = {
+
+        var minv = Int.MaxValue
+        var maxv = Int.MinValue
+        var total = 0
+        val numMap = new HashMap[Int, Int]().withDefaultValue(0)
+
+        nums.foreach(x =>  {
+            minv = math.min(minv, x)
+            maxv = math.max(maxv, x)
+            total+=1
+            numMap(x) += 1
+        }
+        )
+        // println(minv, maxv, total, numMap)
+        for(x <- minv to maxv)
+        {
+            if(numMap.contains(x)){
+                numMap.remove(x)
+            }
+            else {
+                numMap(x) += 1
+                }
+        }
+
+        return List.range(1, minv) ++ numMap.keySet.toList ++ List.range(maxv+1, total+1)
+    }
+
+   val x = findDisappearedNumbers(Array(3,3,3))
+   println(x)
+
+   val x1 = findDisappearedNumbers(Array(1,1))
+   println(x1)
+   
+}
