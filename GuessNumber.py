835118440a2a8db57efc472d0f144c047837352d
smahamkl <tzones2006@gmail.com>
2022-02-09 20:02:15 -0800

    added comments in guess number problem

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,6 +1,21 @@
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+'''
+LeetCode - 374
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick)
+1: Your guess is lower than the number I picked (i.e. num < pick)
+0: your guess is equal to the number I picked (i.e. num == pick)
+Return the number that I picked.
+'''
 def guess(num: int) -> int:
     n = 9
     if num == n:
